bb6446b84bf377ffd5ca9ec3fbadc20ca42113f6
Isabel TS <izzytons@gmail.com>
2021-05-12 12:22:57 -0400

    copy solution for few remaining errors

--- js/sight.js
+++ js/sight.js
@@ -9,7 +9,7 @@ class SVGElement {
 
   attr(attrs) {
     for (const [key, value] of Object.entries(attrs)) {
-      this.node.setAttributeNS(this.namespace, this.type, value);
+      this.node.setAttributeNS(null, key, value);
     }
     return this;
   }
